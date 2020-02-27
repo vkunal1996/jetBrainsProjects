405db90581329fa1f72077af1f7e121fdcbbb76e
Kunal Verma <vkunal1996@gmail.com>
2020-02-27 02:32:51 -0800

    Flashcard Stage4 Started

--- flashcards/Main.java
+++ flashcards/Main.java
@@ -1,6 +1,15 @@
-package flashcards;
 import java.util.*;
 public class Main {
+
+    public static String choice;
+    public static int number;
+    public static Set <String> cardset=new HashSet<>();
+    public static Set<String> definitionset=new HashSet<>();
+    public static Map <String,String> m=new LinkedHashMap<>();
+    public static String card,definition;
+    
+    public static Scanner input=new Scanner(System.in);
+
     public static <K,V> K getKey(Map<K,V>map,V value){
         for(Map.Entry<K,V> entry:map.entrySet()){
             if(value.equals(entry.getValue())){
@@ -9,12 +18,98 @@ public class Main {
         }
         return null;
     }
+    /**Adding card to the Hashmap*/
+
+    public static void addcard(){
+        System.out.println("The card:");
+
+           do{
+            card=input.nextLine();
+           if(cardset.contains(card)){
+               System.out.println("The card "+"\""+card+"\" already exists. Try again:");
+               return;
+           }
+           else{
+               cardset.add(card);
+               break;
+           }
+           }
+           while(true);
+
+           System.out.println("The definition of the card:");
+
+           do{
+            definition=input.nextLine();
+           if(definitionset.contains(definition)){
+                System.out.println("The definition "+"\""+definition+"\" already exists. Try again:");
+                return;
+           }
+           else{
+               definitionset.add(definition);
+               break;
+           }
+           }
+           while(true);
+           m.put(card,definition);
+           System.out.println("The pair (\""+card+" : "+definition+"\") has been added.");
+        
+    }
+
+    public static void remove(){
+        String answer=null;
+        System.out.println("The Card:");
+        answer=input.nextLine();
+        if(m.containsKey(answer)==true){
+            m.remove(answer);
+            System.out.println("The card has been removed.");
+        } 
+        else{
+            System.out.println("Can't remove \""+answer+"\": there is no such card.");
+        }
+    }
+
+    public static void ask(){
+        String answer=null;
+      
+
+        for (String key : m.keySet()){
+            System.out.println("Print the definition of "+"\""+key+"\"");
+            answer=input.nextLine();
+            if(m.get(key).equals(answer)){
+                System.out.println("Correct answer.");
+            }
+            else{
+                System.out.println("Wrong answer. The Correct one is "+"\""+m.get(key)+"\" , you've just written the definition of "+"\""+getKey(m,answer)+"\"");
+            }
+        }
+    }
     public static void main(String[] args) {
-       Scanner input=new Scanner(System.in);
-       int number;
        
-       Set <String> cardset=new HashSet<>();
-       Set<String> definitionset=new HashSet<>();
+
+       do{
+           System.out.println("Input the action (add, remove, import, export, ask, exit):");
+           choice=input.nextLine();
+           switch(choice){
+               case "add":
+               addcard();
+               break;
+               case "remove":
+               remove();
+               break;
+               case "import":
+               break;
+               case "export":
+               break;
+               case "ask":
+               ask();
+               break;
+               case "exit":
+               System.out.println("Bye bye!");
+               break;
+            }
+
+       }while(!choice.equals("exit"));
+       /**
        System.out.println("Input the number of cards:");
        number=input.nextInt();
        
@@ -70,6 +165,6 @@ public class Main {
             else{
                 System.out.println("Wrong answer. The Correct one is "+"\""+m.get(key)+"\" , you've just written the definition of "+"\""+getKey(m,answer)+"\"");
             }
-        }
+        } */
     }
 }
